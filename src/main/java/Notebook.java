a9a7cb95a4882d281b68cd4e19cc2efb48e170fb
DreamFlower <sunghh1004@naver.com>
2022-03-16 17:49:29 +0900

    notebook

--- src/main/java/Notebook.java
+++ src/main/java/Notebook.java
@@ -1,5 +1,98 @@
+import java.util.Scanner;
+
 public class Notebook {
     public static void main(String[] args) {
-        System.out.println("ppt에 있는 단어들 무조건 외우기");
+        int deaip = 2;
+        /* deaip += 4;
+        System.out.printf("더함 = %d\n", deaip);
+        deaip -= 4;
+        System.out.printf("뺌 = %d\n", deaip);
+        deaip *= 8;
+        System.out.printf("곱함 = %d\n", deaip);
+        deaip /= 4;
+        System.out.printf("나눔 = %d\n", deaip);
+
+        deaip %= 3;
+        System.out.printf("나머지 = %d\n", deaip);  // 왜 2?
+
+        System.out.println(2%3);
+
+        int num = 1;
+        // 선 참조 후 연산
+        System.out.println(num++);
+        // 선 연산 후 참조
+        System.out.println(++num);
+
+        int a = 1, b = 2, c = 0;
+
+        System.out.println( a + " 단 " );
+        System.out.println( a * ++c );
+        System.out.println( a * ++c );
+        System.out.println( a * ++c );
+        System.out.println( a * ++c );
+        System.out.println( a * ++c );
+        System.out.println( a * ++c );
+        System.out.println( a * ++c );
+        System.out.println( a * ++c );
+        System.out.println( a * ++c );
+
+        c = 0;
+
+        System.out.println( b + " 단 " );
+        System.out.println( b*(++c));
+        System.out.println( b*(++c));
+        System.out.println( b*(++c));
+        System.out.println( b*(++c));
+        System.out.println( b*(++c));
+        System.out.println( b*(++c));
+        System.out.println( b*(++c));
+        System.out.println( b*(++c));
+        System.out.println( b*(++c));
+
+        int a = 3, b = 4, c = 0;
+        System.out.println( a + " 3 ");
+        System.out.println( a + " * " + (++c) + " = " + a * c );
+        System.out.println( a + " * " + (++c) + " = " + a * c );
+        System.out.println( a + " * " + (++c) + " = " + a * c );
+        System.out.println( a + " * " + (++c) + " = " + a * c );
+        System.out.println( a + " * " + (++c) + " = " + a * c );
+        System.out.println( a + " * " + (++c) + " = " + a * c );
+        System.out.println( a + " * " + (++c) + " = " + a * c );
+        System.out.println( a + " * " + (++c) + " = " + a * c );
+        System.out.println( a + " * " + (++c) + " = " + a * c ); */
+
+        Scanner in = new Scanner(System.in); // 선언, 초기화
+        System.out.print(" 이름 입력 : ");
+        String name = "김성현";
+        String inputName = in.nextLine();
+        if(name.equals(inputName)) {
+            System.out.print("몇 단 : ");
+            int a = in.nextInt();
+            int b = 0;
+            System.out.println(a + " * " + (++b) + " = " + a * b);
+            System.out.println(a + " * " + (++b) + " = " + a * b);
+            System.out.println(a + " * " + (++b) + " = " + a * b);
+            System.out.println(a + " * " + (++b) + " = " + a * b);
+            System.out.println(a + " * " + (++b) + " = " + a * b);
+            System.out.println(a + " * " + (++b) + " = " + a * b);
+            System.out.println(a + " * " + (++b) + " = " + a * b);
+            System.out.println(a + " * " + (++b) + " = " + a * b);
+            System.out.println(a + " * " + (++b) + " = " + a * b); }
+        else {
+                System.out.println(" 로그인 실패 ");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
