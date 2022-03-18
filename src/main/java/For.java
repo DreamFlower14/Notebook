afca7759b5a9fa10d0b77def3789634e516ac122
DreamFlower <sunghh1004@naver.com>
2022-03-18 16:23:22 +0900

    notebook

---
+++ src/main/java/For.java
@@ -0,0 +1,27 @@
+public class For {
+    public static void main(String[] args){
+        //  반복문
+        // for(초기값(1번째); 조건(2번째); 증감식(4번째)){ 반복 작업내용(3번째) } 조건에서 성공했을때만 3번으로 간다.
+
+        /* for(int a = 0; a < 3; ++a){
+            System.out.println( (a+1) + "번째 출력"); */
+
+        /* Scanner A = new Scanner(System.in);     // Scanner를 이용한 반복출력
+        System.out.print("몇 번 반복할까요? : ");
+        int B = A.nextInt();
+        for( int a = 1; a <= B; a++){
+            System.out.println(a + "번쨰 반복"); */
+
+        /* Scanner in = new Scanner(System.in);     // Scanner를 이용한 구구단 출력
+        System.out.print(" 몇 단 : ");
+        int b = in.nextInt();
+        System.out.println( b + "단");
+        for( int a = 1; a < 10; a++){
+            System.out.println( b + " * " + a + " = " + a*b );   */
+
+
+        }
+        }
+
+
+
---
+++ src/main/java/SecondFor.java
@@ -0,0 +1,30 @@
+public class SecondFor {
+    public static void main(String[] args){
+
+       /* int set = 3;
+        int reps = 5;
+        for(int i = 1; i <= set; i++){
+            for(int j = 1; j <= reps; j++){
+                System.out.print(i + "세트 " + j + "회 ");
+            }
+            System.out.println();
+        }
+            for(int a = 1; a <= 9; a++){
+            if( a % 2 == 1){ System.out.println( "<" + a + "단>");}  // "="은 대입연산자고 같다는 뜻을 쓰려면 "=="을 사용해야 한다.
+            else{
+                System.out.println(a +"단");  // 짝수 구구단에는 "단", 홀수단에는 "<단>"을 사용한다.
+            }
+            for(int b = 1; b <= 9; b++){
+                System.out.println(a + " X " + b + " = " + (a*b));
+            }
+            System.out.println(); } */
+
+
+
+
+
+ }
+}
+
+
+
